7d9743ebf5a13b3cb54df7176f95f52d7e7f4f70
sebastien06400 <69351095+sebastien06400@users.noreply.github.com>
2020-12-15 06:21:03 -0800

    Add files via upload

--- 30projets/1/script.js
+++ 30projets/1/script.js
@@ -6,11 +6,13 @@
 // function keyListener(key) {
 
 // }
-
-
-window.addEventListener('keydown', playMusic)
-
+const lyricsContents = Array.from(document.querySelectorAll(".lyrics"));
+const filterBox = document.querySelector(".filter");
+const musicBoxes = Array.from(document.querySelectorAll(".songbox"));
 const musics = document.querySelectorAll("audio");
+const stop = document.querySelector(".stop");
+const notice = document.querySelector(".notice");
+
 
 function playMusic(e) {
     let div = document.querySelector(`div[data-key="${e.keyCode}"]`) 
@@ -19,26 +21,42 @@ function playMusic(e) {
     stopMusics()
     music.play()
     div.classList.add("playing")
-}
+    notice.classList.add("hidden")
+    let content = document.querySelector(`div[lyrics-key="${e.keyCode}"]`) 
+    content.classList.add("display")
+    filterBox.classList.add("stretch")
+};
+
 
 function stopMusics() {
     musics.forEach(music => music.pause());
     musicBoxes.forEach(box => box.classList.remove("playing"));
-
-}
+    filterBox.classList.remove("stretch")
+    lyricsContents.forEach(content => content.classList.remove("display"))
+    notice.classList.remove("hidden")
+};
 
 // stop button
-const stop = document.querySelector(".stop");
 stop.addEventListener('click', () => {
   stopMusics()
 });
 
-
-const musicBoxes = Array.from(document.querySelectorAll(".songbox"));
+// event play music on click
 musicBoxes.forEach(box => box.addEventListener('click', () => {
     let music = document.querySelector(`audio[data-key="${box.attributes[1].value}"]`) 
     stopMusics()
     music.play()
     box.classList.add("playing")
+    let content = document.querySelector(`div[lyrics-key="${box.attributes[1].value}"]`) 
+    content.classList.add("display")
+    setTimeout(displayLyric, 400, content)
+    filterBox.classList.add("stretch")
+    notice.classList.add("hidden")
   })
 );
+
+function displayLyric(content) {
+  content.classList.add("delayed-display")
+};
+
+window.addEventListener('keydown', playMusic);
